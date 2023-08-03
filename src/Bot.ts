0082cad54f75ce99e96767dd6f7df004285fe237
andrea <andrea.batino@oracle.com>
2023-08-03 11:19:47 +0200

    added gitignore

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,5 +1,9 @@
 import { Client } from "discord.js";
 
+require('dotenv').config(); // Load the .env file
+// Access the token
+const token = process.env.DISCORD_BOT_TOKEN;
+
 console.log("Bot is starting...");
 
 const client = new Client({
