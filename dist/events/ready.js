955150aa27c350b745d561acf8c88dc38468a361
ntefa <andrea.batino94@gmail.com>
2023-09-04 16:01:56 +0100

    Remove dist from Git tracking

--- dist/events/ready.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (client) => {
-    client.once('ready', () => {
-        console.log(`Ready! Logged in as ${client.user?.tag}`);
-    });
-};
